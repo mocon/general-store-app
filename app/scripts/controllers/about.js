6646c02652a47aa9b0c2d1c7e40630a356820c9b
Myles O'Connor <myles@drocventures.com>
2015-11-17 07:40:52 -0800

    Initial commit of Yeoman Angular generated boilerplate

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name generalStoreApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the generalStoreApp
+ */
+angular.module('generalStoreApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
