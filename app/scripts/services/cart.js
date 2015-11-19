cdb9f8bf0eb1b6c954c68c37d88d2ba3f900e8d3
Myles O'Connor <myles@drocventures.com>
2015-11-19 07:49:18 -0800

    Added a few methods to CartService

--- app/scripts/services/cart.js
+++ app/scripts/services/cart.js
@@ -13,8 +13,31 @@ angular.module('generalStoreApp')
     
 	  this.cartItems = [];
 	  
+	  this.getCartItems = function(){
+		  return this.cartItems;
+	  };
+	  
 	  this.incrementCartCounter = function(){
 		  $rootScope.numberOfItemsInCart ++;
 	  };
+	  
+	  this.decrementCartCounter = function(){
+		  $rootScope.numberOfItemsInCart --;
+	  };
+	  
+	  this.cartSubtotal = function(){
+	    var itemsInCart = $rootScope.allProducts,	    
+	    		i,
+	    		runningTotal = 0;
+	    		
+	    for (i in itemsInCart){
+		    if (itemsInCart[i].hasOwnProperty('qtyInCart')) {
+			    runningTotal += itemsInCart[i].qtyInCart * itemsInCart[i].price;
+		    }
+	    }
+	    $rootScope.subtotal = runningTotal;
+	    $rootScope.tax = $rootScope.subtotal * 0.095;
+	    $rootScope.total = $rootScope.subtotal + $rootScope.tax;
+    };
     
   });
